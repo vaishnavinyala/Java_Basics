3bb26d36c17b5459bd67e1b895c6758273c79f6b
Vaishnavi <vaishnavinyala8143@glmail.com>
2025-11-05 22:20:28 +0530

    basic of the java

---
+++ src/Map/hash.java
@@ -0,0 +1,15 @@
+package Map;
+import java.util.HashMap;
+    public class hash {
+        public static  void main(String[] args) {
+            HashMap<String,Integer> fruits =new HashMap<String,Integer>();
+            fruits.put( "apple",1);
+            fruits.put("banana",23);
+            fruits.put("mango",81);
+            System.out.println(fruits);
+        }
+
+    }
+
+
+
